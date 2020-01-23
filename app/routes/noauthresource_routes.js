46c76152fdb225a8cdf85ac671da5cc688c5b170
charles <deltin00@gmail.com>
2020-01-22 23:33:31 -0500

    moded router to fix options issue

--- app/routes/noauthresource_routes.js
+++ app/routes/noauthresource_routes.js
@@ -4,6 +4,11 @@ const handle = require('../../lib/error_handler')
 // const customErrors = require('../../lib/custom_errors')
 // const handle404 = customErrors.handle404
 const router = express.Router()
+router.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://acharles001.gitlab.io/investbuddy/') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 router.get('/noauthresource', (req, res) => {
   noauthresource.find()
@@ -15,7 +20,6 @@ router.get('/noauthresource', (req, res) => {
 })
 
 router.post('/noauthresource', (req, res) => {
- 
   noauthresource.create(req.body.noauthresource)
     .then(noauthresource => {
       res.status(201).json({ noauthresource: noauthresource.toObject() })
