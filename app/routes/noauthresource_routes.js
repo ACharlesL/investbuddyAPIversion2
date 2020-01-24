882c3a4bf90a45c07dd213fe6ff7970aa94b1d9f
charles <deltin00@gmail.com>
2020-01-23 22:16:18 -0500

    moded router to fix options issue

--- app/routes/noauthresource_routes.js
+++ app/routes/noauthresource_routes.js
@@ -1,17 +1,17 @@
 const express = require('express')
-const noauthresource = require('../models/noauthresource')
+const Noauthresource = require('../models/noauthresource')
 const handle = require('../../lib/error_handler')
 // const customErrors = require('../../lib/custom_errors')
 // const handle404 = customErrors.handle404
 const router = express.Router()
 router.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://acharles001.gitlab.io/investbuddy/') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://acharles001.gitlab.io/investbuddy ') // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
 router.get('/noauthresource', (req, res) => {
-  noauthresource.find()
+  Noauthresource.find()
     .then(noauthresource => {
       return noauthresource.map(noauthresource => noauthresource.toObject())
     })
@@ -20,7 +20,7 @@ router.get('/noauthresource', (req, res) => {
 })
 
 router.post('/noauthresource', (req, res) => {
-  noauthresource.create(req.body.noauthresource)
+  Noauthresource.create(req.body.noauthresource)
     .then(noauthresource => {
       res.status(201).json({ noauthresource: noauthresource.toObject() })
     })
